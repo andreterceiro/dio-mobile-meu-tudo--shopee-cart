0c07416a8fd32718f283f1c51ef7952288225fa5
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2025-07-22 18:36:48 -0300

    feat: in index.js we created 2 items and calculate one subtotal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("index.js");
+import createItem from './services/item.js';
+
+console.log("Bem-vindo ao carrinho da Shopee");
+
+const item1 = await createItem("bala", 0.25, 4);
+const item2 = await createItem("pirulito", 1, 1);
+
+console.log(item1.subtotal());
