215678bd61762607185a19cf8e86136e5c2f86f7
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2025-07-22 18:57:12 -0300

    feat: using the cart service

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import createItem from './services/item.js';
 
 console.log("Bem-vindo ao carrinho da Shopee");
 
+const myCart = [];
+
 const item1 = await createItem("bala", 0.25, 4);
 const item2 = await createItem("pirulito", 1, 1);
 
-console.log(item1.subtotal());
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+console.log("Subtotal: " + await cartService.calculateTotal(myCart));
+
