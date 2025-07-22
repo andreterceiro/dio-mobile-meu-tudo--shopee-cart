eeb32d472e6373ab9d4b128d11b40c478fc6046e
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2025-07-22 19:11:10 -0300

    feat: implementing the function that can remove one item from a cart

--- src/services/cart.js
+++ src/services/cart.js
@@ -2,7 +2,12 @@ async function addItem(userCart, item) {
     userCart.push(item);
 }
 
-async function deleteItem(userCart, name){}
+async function deleteItem(userCart, name){
+    const index = userCart.findIndex((item) => item.name === name);
+    if (index != -1) {
+        userCart.splice(index, 1);
+    } 
+}
 
 async function removeItem(userCart, index){}
 
